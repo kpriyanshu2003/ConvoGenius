9e725607ec6a0473651543f05dfd4205292d2e92
kpriyanshu2003 <az.priyanshu.rockstar@gmail.com>
2024-03-22 15:08:45 +0530

    Setup Prisma

---
+++ prisma/connectDb.ts
@@ -0,0 +1,13 @@
+import { PrismaClient } from "@prisma/client";
+declare const global: typeof globalThis & { db?: PrismaClient };
+
+let db;
+
+if (process.env.NODE_ENV === "production") {
+  db = new PrismaClient();
+} else {
+  if (!global.db) global.db = new PrismaClient();
+  db = global.db;
+}
+
+export { db };
