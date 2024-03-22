8f2577da610708e1f5418ffea3aa9b36231f7d5f
kpriyanshu2003 <az.priyanshu.rockstar@gmail.com>
2024-03-22 15:09:21 +0530

    Configuring NextAuth

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
 
+## Prisma
+
+npx prisma migrate dev --name name_here && yarn add -D prisma @prisma/client
+
 ## Getting Started
 
 First, run the development server:
--- src/app/api/[...nextauth]/route.ts
+++ src/app/api/[...nextauth]/route.ts
@@ -1,14 +1,85 @@
-import NextAuth from "next-auth"
-import { SupabaseAdapter } from "@auth/supabase-adapter"
+import NextAuth from "next-auth";
+import Google from "next-auth/providers/google";
+import CredentialsProvider from "next-auth/providers/credentials";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { PrismaClient } from "@prisma/client";
 
-// For more information on each option (and a full list of options) go to
-// https://authjs.dev/reference/core#authconfig
-export default NextAuth({
-  // https://authjs.dev/reference/core/providers
-  providers: [...],
-  adapter: SupabaseAdapter({
-    url: process.env.NEXT_PUBLIC_SUPABASE_URL,
-    secret: process.env.SUPABASE_SERVICE_ROLE_KEY,
-  }),
-  // ...
-})
+const prisma = new PrismaClient();
+
+// TODO : Add Google Auth Creds
+export const { handlers, auth, signIn, signOut } = NextAuth({
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    CredentialsProvider({
+      // The name to display on the sign in form (e.g. "Sign in with...")
+      name: "Credentials",
+      // `credentials` is used to generate a form on the sign in page.
+      // You can specify which fields should be submitted, by adding keys to the `credentials` object.
+      // e.g. domain, username, password, 2FA token, etc.
+      // You can pass any HTML attribute to the <input> tag through the object.
+      credentials: {
+        username: { label: "Username", type: "text", placeholder: "jsmith" },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials, req) {
+        // Add logic here to look up the user from the credentials supplied
+        const user = { id: "1", name: "J Smith", email: "jsmith@example.com" };
+
+        if (user) {
+          // Any object returned will be saved in `user` property of the JWT
+          return user;
+        } else {
+          // If you return null then an error will be displayed advising the user to check their details.
+          return null;
+
+          // You can also Reject this callback with an Error thus the user will be sent to the error page with the error message as a query parameter
+        }
+      },
+    }),
+    Google({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+  ],
+  callbacks: {
+    async jwt({ token, user }) {
+      if (user) {
+        return {
+          ...token,
+          name: user.name,
+          id: user.id,
+        };
+      }
+      return token;
+    },
+
+    async signIn({ user }) {
+      const existingUser = await db.user.findFirst({
+        where: {
+          id: user.id,
+        },
+      });
+
+      if (!existingUser?.emailVerified) {
+        return false;
+      }
+      return true;
+    },
+
+    async session({ session, token, user }) {
+      return {
+        ...session,
+        user: {
+          ...session.user,
+          name: token.name,
+          id: token.id,
+        },
+      };
+    },
+  },
+  pages: {
+    signIn: "/sign-in",
+  },
+});
+
+export { handlers as GET, handlers as POST };
